11f519f2168f8b00c7044c4fd0cc92241c9abe0e
generusngasinan@gmail.com <hilalhamdihafit@gmail.com>
2025-08-05 23:30:14 +0800

    add file

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -171,7 +171,7 @@ const AdminDashboard = () => {
                 type="date"
                 value={dateFrom}
                 onChange={(e) => setDateFrom(e.target.value)}
-                className="input w-full sm:w-auto min-w-[150px] bg-white border rounded px-3 py-2"
+                className="input w-full sm:w-auto min-w-[150px] bg-white border border-gray-700 rounded px-3 py-2"
               />
             </div>
 
@@ -183,7 +183,7 @@ const AdminDashboard = () => {
                 type="date"
                 value={dateTo}
                 onChange={(e) => setDateTo(e.target.value)}
-                className="input w-full sm:w-auto min-w-[150px] bg-white border rounded px-3 py-2 text-gray-700"
+                className="input w-full sm:w-auto min-w-[150px] bg-white border border-gray-700 rounded px-3 py-2 text-gray-700"
               />
             </div>
 
@@ -196,7 +196,7 @@ const AdminDashboard = () => {
                 value={searchName}
                 onChange={(e) => setSearchName(e.target.value)}
                 placeholder="Cari nama pasien/personil"
-                className="input w-full bg-white border rounded px-3 py-2 text-gray-700"
+                className="input w-full bg-white border border-gray-700 rounded px-3 py-2 text-gray-700"
               />
             </div>
             <button
